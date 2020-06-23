48246d603e6a4e316426e38cf5d7f30ecfd8859a
mathewpoovathany <64447136+mathewpoovathany@users.noreply.github.com>
2020-06-23 16:35:12 -0400

    Update code.js
    Added local storage

--- code.js
+++ code.js
@@ -1,96 +1,79 @@
-/*
 
-$("#currentDay").text(moment().format('ddd, MMM Do'));
+var time9am = document.querySelector("#am9text");
+var button9am = document.querySelector("#am9Button");
 
-//currentTime to determine if the hour matchs for color
-var currentTime = moment().format('H');
-console.log(moment())
+var time10am = document.querySelector("#am10text");
+var button10am = document.querySelector("#am10Button");
 
-//todo project
-// Use window.localStoirage to retreve and store your data object as string
-const LS = JSON.parse(localStorage.scheduler || '{}'); // now an Object
+var time11am = document.querySelector("#am11text");
+var button11am = document.querySelector("#am11Button");
 
-// double click makes calender content editable
+var time12pm = document.querySelector("#pm12text");
+var button12pm = document.querySelector("#pm12Button");
 
-$(".textData").dblclick(function () {
-    $(this).addClass('d-none')
-    $(this).siblings("input").removeClass("d-none");
+var time1pm = document.querySelector("#pm1text");
+var button1pm = document.querySelector("#pm1Button");
+
+var time2pm = document.querySelector("#pm2text");
+var button2pm = document.querySelector("#pm2Button");
+
+var time3pm = document.querySelector("#pm3text");
+var button3pm = document.querySelector("#pm3Button");
+
+var time4pm = document.querySelector("#pm4text");
+var button4pm = document.querySelector("#pm4Button");
+
+var time5pm = document.querySelector("#pm5text");
+var button5pm = document.querySelector("#pm5Button");
+
+
+button9am.addEventListener("click", function () {
+  localStorage.setItem("textam9",document.getElementById("am9text").value);
+  alert("The value you entered at 9am is: " + localStorage.getItem("textam9")+"  Saved Successafully!");;
 });
 
-$(".btn").dblclick(function () {
-    console.log(this);
-    console.log($(this).attr("id"))
-    var id = $(this).attr("id")
-    var time = id.slice(0,3)
-    console.log(time)
+button10am.addEventListener("click", function () {
+  localStorage.setItem("textam10",document.getElementById("am10text").value);
+  alert("The value you entered at 10 am is: " + localStorage.getItem("textam10")+"  Saved Successafully!");;
+});
 
-    var timeInputId = time + "Input"; 
+button11am.addEventListener("click", function () {
+  localStorage.setItem("textam11",document.getElementById("am11text").value);
+  alert("The value you entered at 11 am is: " + localStorage.getItem("textam11")+"  Saved Successafully!");;
+});
+
+button12pm.addEventListener("click", function () {
+  localStorage.setItem("textpm12",document.getElementById("pm12text").value);
+  alert("The value you entered at 12 pm is: " + localStorage.getItem("textpm12")+"  Saved Successafully!");;
+});
+
+button1pm.addEventListener("click", function () {
+  localStorage.setItem("textpm1",document.getElementById("pm1text").value);
+  alert("The value you entered at 1 pm is: " + localStorage.getItem("textpm1")+"  Saved Successafully!");;
+});
+
+button2pm.addEventListener("click", function () {
+  localStorage.setItem("textpm2",document.getElementById("pm2text").value);
+  alert("The value you entered at 2 pm is: " + localStorage.getItem("textpm2")+"  Saved Successafully!");;
+});
+
+button3pm.addEventListener("click", function () {
+  localStorage.setItem("textpm3",document.getElementById("pm3text").value);
+  alert("The value you entered at 3 pm is: " + localStorage.getItem("textpm3")+"  Saved Successafully!");;
+});
+
+button4pm.addEventListener("click", function () {
+  localStorage.setItem("textpm4",document.getElementById("pm4text").value);
+  alert("The value you entered at 4 pm is: " + localStorage.getItem("textpm4")+"  Saved Successafully!");;
+});
+
+button5pm.addEventListener("click", function () {
+  localStorage.setItem("textpm5",document.getElementById("pm5text").value);
+  alert("The value you entered at 5 pm is: " + localStorage.getItem("textpm5")+"  Saved Successafully!");;
 });
 
 
-
-
-
-// save makes edit end
-
-const rows = document.getElementsByClassName("row");
-let currentHour = parseInt(moment().format('H'));
-
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
-    }
-  }
-});
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
-
-// set active time
-
-
-
-// make background color of / past hours gray / current hour red / future hours green
-var hourPast = hourPast < currentTime;
-var hourNow = currentTime;
-var hourFuture = hourFuture < currentTime;
-
-// if currentTime > hourNow make background color gray//
-
-
-    var hourNow = currentTime;
-console.log(hourNow)
-console.log(currentTime)
-    function backgroundColorEdit(){
-        if (hourNow == currentTime){
-            document.getElementsByClassName("backgroundTool").style.backgroundColor = "#ff0000";
-        }else if (hourNow < currentTime){
-            document.getElementsByClassName("backgroundTool").style.backgroundColor = "#d3d3d3";
-        }else 
-        {
-            document.getElementsByClassName("backgroundTool").style.backgroundColor = "#90ee90";
-        }
-} */
-// if currentTime = hourNow make background color Red //
-
-// if currentTime < hourNow make background color green //
-
-
+// time maintainance
 const rows = document.getElementsByClassName("row");
 
 let currentHour = parseInt(moment().format('H'));
